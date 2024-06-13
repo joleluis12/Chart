71992c27e35bf2266a78423cb01ed5677dee45e9
joleluis12 <152332326+joleluis12@users.noreply.github.com>
2024-06-13 14:48:35 -0700

    Chart

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import BarChart from './Charts/BarChart';
+import DoughnutChart from './Charts/DoughnutChart';
+import LineChart from './Charts/LineChart';
+import PieChart from './Charts/PieChart';
+
+function App() {
+  return (
+    <div className="App">
+      <BarChart />
+    </div>
+  );
+}
+
+export default App;
